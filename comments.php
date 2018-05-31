a61b704a27bbcb16c2e362612f4f65c3e8dbdd30
wjs <w1014221775@126.com>
2018-06-01 01:09:55 +0800

    add comments

---
+++ comments.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Comments</title>
+    </head>
+    <body>
+        <?php echo $_POST["username"]; ?><br>
+        <?php echo $_POST["email"]; ?><br>
+        <?php echo $_POST["comment"]; ?>
+    </body>
+</html>
